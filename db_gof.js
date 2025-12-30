868b579a707c3826186b9002b18cba1982a7a4bb
bbdjk <bbdjk06022@gmail.com>
2025-12-30 22:36:46 +0900

    Update db_gof.js

--- db_gof.js
+++ db_gof.js
@@ -14,8 +14,18 @@ const DATA_GOF = [
         ]
     },
     { 
+        id:'gof_ability', type:'rule', cat:'GOF', title:'🟥 GOF의 게임 방식', 
+        desc:'Guardians of Freedom Special Abilit',
+        data:[
+            {q:'드론의 커맨드를 지시하면 두 가지 액션중 하나를 수행합니다.', a:'1. 1회 이동 수행, 2. 1개의 커맨드 액션 수행'},
+            {q:'드론에 커맨드를 지시하려면 토르소 부품 카드에서 커맨드 토큰을 가져옵니다.', a:'커맨드 토큰을 가져와 드론의 카드 또는 베이스에 뒷면이 보이도록 놓습니다.'},
+            {q:'커맨드 페이즈에 드론들에게 커맨드를 1개씩 전달합니다.', a:'액션 페이즈때 커맨드 조정이라는 키워드에 따라 액션을 수행합니다. 단, 드론1대에 2번 액션은 할 수 없습니다.'}
+        ]
+    },
+    { 
         id:'gof_sample', type:'unit', cat:'GOF', title:'GOF Sample Unit', sub:'Torso', color:'gof',
         stats:{Armor:0, Struct:0, EW:0},
         details:[{h:'[샘플]', t:'추가 GOF 데이터가 있다면 이곳에 입력됩니다.'}]
     }
 ];
+
